9335485e1153caa18019402c6b54d904ca1f3dba
Rafael <rpena@sfu.ca>
2016-11-03 19:54:39 -0700

    added file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
+  get "/searchdrink", to: 'welcome#drink', as: "drink_search"
+  get "/404", to: 'errors#not_found', as: "error_404"
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
 
