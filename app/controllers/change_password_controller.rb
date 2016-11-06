aa717693e77eaec03387f07aad0add17405251cf
yongzhel <yongzhel@sfu.ca>
2016-11-06 00:08:11 -0700

    alert messages

--- app/controllers/change_password_controller.rb
+++ app/controllers/change_password_controller.rb
@@ -1,11 +1,22 @@
 class ChangePasswordController < ApplicationController
 def change
-  @password = params[:Password]
+  @password = params[:password]
+  @password_confirmation = params[:password_confirmation]
+  @old_password = params[:old_password]
   @user = User.find_by_Username (params[:Username])
   if @user
-    @user.update_attributes(Password: @password)
+    if @old_password = @user.Password
+      if @password = @password_confirmation
+        @user.update_attributes(Password: @password)
+        flash.alert = "Password has been updated"
+      else
+        flash.alert = "Invalid Password Confirmation"
+      end
+    else
+      flash.alert = "Invalid Username or Old Password"
+    end
   else
-    flash.now[:danger] = "Username not found"
+    flash.alert = "Invalid Username or Old Password"
   end
 end
 def user_params
