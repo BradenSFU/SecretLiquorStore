9546c05f8ee0106121027a6fe973fff53afbe9b8
Kevin Chung <kca99@sfu.ca>
2016-11-14 15:54:11 -0800

    added ingredients to random drink page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "/searchdrink", to: 'welcome#drink', as: "drink_search"
+
   get "/404", to: 'errors#not_found', as: "error_404"
 
   get "just_logged_in", to: 'welcome#just_logged_in', as: "just_logged_in"
