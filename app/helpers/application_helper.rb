18e9ce981e9163dea27e445a487fb7e1d8704632
Rafael <rpena@sfu.ca>
2016-11-19 19:55:33 -0800

    Set up revision stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,23 @@
 module ApplicationHelper
 
   def flash_message
-    flash.map do |key, msg|
-      content_tag :div, msg, :id => key, :class => 'flash'
-    end.join
+    #flash.map do |key, msg|
+    #  content_tag :div, msg, :id => key, :class => 'flash'
+    #end.join
   end
 
   def add_to_likes(drink)
     #flash[:color] = green
-    flash[:notice] = "You have added " + drink['strDrink'] + " to your likes"
-    #current_user.likes << drink
-    #current_user.likes.index(current_user.likes.size-1).islike = true
+    #flash[:notice] = "You have added " + drink['strDrink'] + " to your likes"
+    #Find drink by id
+    #like = Like.new("islike" => "true", "drink_id" => "1", "user_id" => "1")
   end
 
   def add_to_dislikes(drink)
     #flash[:color] = red
-    flash[:notice] = "You have added " + drink['strDrink'] + " to your dislikes"
-    #current_user.likes << drink
-    #current_user.likes.index(current_user.likes.size-1).islike = false
+    #flash[:notice] = "You have added " + drink['strDrink'] + " to your dislikes"
+    #Find drink by id
+    #like = Like.new("islike" => "false", "drink_id" => "1", "user_id" => "1")
   end
 
   def remove_from_likes(drink)
