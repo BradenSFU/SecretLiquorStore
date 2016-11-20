1e25d9a3fb4c2684b6515b105ec826dbaba3741e
yongzhel <yongzhel@sfu.ca>
2016-11-19 16:49:11 -0800

    Init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104034311) do
+ActiveRecord::Schema.define(version: 20161108015840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20161104034311) do
     t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "structures", force: :cascade do |t|
   end
 
   create_table "things", force: :cascade do |t|
