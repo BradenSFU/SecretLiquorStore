adba3e0d3f75b64acbdb30a5dffd4da67d06df8d
DoubleLLLL <yongzhel@sfu.ca>
2016-11-20 15:41:13 -0800

    try again

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :likes
   resources :publishes
-  match "log_in" => "sessions#new", :as => "log_in", via: [:get, :post]
-  
+  get "log_in" => "sessions#new", :as => "log_in"
+  post "log_in" => "session#new", :as => "session"
+
   get "/drinks/searched" => 'drinks#drinksearch', as: 'drink_search'
   get "/drinks/show", to: 'drinks#show', as: "drink_show"
   match "/drinks/search", to: 'drinks#drinkresults', as: 'drink_results', via: [:get, :post]
