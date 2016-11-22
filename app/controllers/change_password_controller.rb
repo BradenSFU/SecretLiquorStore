8c2aed3b43dee6530ccb0093baec9ae00d5ef590
DoubleLLLL <yongzhel@sfu.ca>
2016-11-21 21:36:30 -0800

    reset password

--- app/controllers/change_password_controller.rb
+++ app/controllers/change_password_controller.rb
@@ -20,7 +20,27 @@ def change
       flash.alert = "Invalid Old Password"
     end
 end
+def reset
+  @email = params[:email]
+  @password = params[:password]
+  @password_confirmation = params[:password_confirmation]
+  @user = User.find_by Email: params[:email]
+  if @user
+    if @password == @password_confirmation
+      if @password.blank? || @password.length <= 5
+        flash.alert = "Password should have at least 6 characters"
+      else
+        @user.update_attributes(Password: @password)
+        redirect_to root_url, :notice => "Password has been updated!"
+      end
+    else
+      flash.alert = "Invalid Password Confirmation"
+    end
+  else
+    flash.alert = "Invalid Email"
+  end
+end
 def user_params
-  params.require(:username, :old_password).permit(:password, :password_confirmation)
+  params.require(:username, :email, :old_password).permit(:password, :password_confirmation)
 end
 end
