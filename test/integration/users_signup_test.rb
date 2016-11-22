9740ed711b918463156972f5ec10a530811c54c3
DoubleLLLL <yongzhel@sfu.ca>
2016-11-21 17:41:58 -0800

    Sign up test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,5 +1,15 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "Can create user" do
+    get "/users/new"
+    assert_response :success
+    post "/users",
+    params: { user: {Username: "can create", Email: "can@email.com", Biography: "I am can create", Password: "123456",
+      Password_confirmation:"123456", Isadmin: +}}
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
 end
 #:Username, :Password, :Password_confirmation, :Email, :Biography, :Isadmin
