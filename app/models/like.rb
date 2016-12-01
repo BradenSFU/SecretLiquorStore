4b9990d1603fe7b16efbcfa1c88c4712206ced06
Rafael <rpena@sfu.ca>
2016-11-30 17:09:00 -0800

    Re-added overwritten data

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :publish
 
+=begin
   def self.create_like(publish,current_user)
     like = Like.find_or_initialize_by(:user_id=>current_user.id)
     like.publish_id = publish.id
@@ -12,19 +13,18 @@ class Like < ApplicationRecord
       like.save
     end
   end
+=end
 
-  def self.create_dislike(publish,current_user)
-    like = Like.find_or_initialize_by(:user_id=>current_user.id)
+  def self.create_vote(publish,current_user,vote)
+    like = Like.new
     like.publish_id = publish.id
-    if !like.islike
-      like.destroy
-    else
-      like.islike = false
-      like.save
-    end
+    like.user_id = current_user.id
+    like.islike = vote
+    like.save
   end
 
   def self.remove_from_likes_list_on_model(publish,current_user)
+    Like.where(:publish_id => publish.id, :user_id => current_user.id).destroy_all
   end
 
 end
