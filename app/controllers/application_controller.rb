80ea1b5e891ad5f0b341646d21a0099f29b9ed72
Rafael <rpena@sfu.ca>
2016-11-30 19:17:52 -0800

    Likes and dislikes finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :add_to_likes
-  def add_to_likes(publish)
-    Like.create_vote(publish,current_user,true)
-  end
-
-  helper_method :add_to_dislikes
-  def add_to_dislikes(publish)
-    Like.create_vote(publish,current_user,false)
-  end
-
   helper_method :remove_from_likes_list
   def remove_from_likes_list(publish)
     Like.remove_from_likes_list_on_model(publish,current_user)
