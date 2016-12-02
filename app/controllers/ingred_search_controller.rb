e796a937d1603ce3f420e517b3239c546829f054
Kevin Chung <kca99@sfu.ca>
2016-12-01 16:12:20 -0800

    DUDE IT WORKS

--- app/controllers/ingred_search_controller.rb
+++ app/controllers/ingred_search_controller.rb
@@ -1,7 +1,7 @@
 class IngredSearchController < ApplicationController
 
   def ingredSearch
-
+=begin
     #params[:ingredient]
     @string = "gin"
     puts @string
@@ -11,7 +11,7 @@ class IngredSearchController < ApplicationController
     uri = URI(url)
     response = Net::HTTP.get(uri)
     parsed = JSON.parse(response)
-    resultsArry= Array.new
+    GlobalArray= Array.new
     if parsed['drinks'] == nil
       redirect_to error_404_url
     else
@@ -22,42 +22,84 @@ class IngredSearchController < ApplicationController
         #puts @firstentry
         @results.each do |item|
           #puts item['strDrink']
-          resultsArry.push([item['strDrink'],counter])
+          GlobalArray.push([item['strDrink'],counter])
         end
-        #puts resultsArry[0] #I HAVE DONE IT NOW I REITERATE HERE USING SECOND STRING
-        #puts resultsArry[1]
-        #puts resultsArry[2]   #[0] for (name,counter) [0][0] for name [0][1] for counter
+        #puts GlobalArray[0] #I HAVE DONE IT NOW I REITERATE HERE USING SECOND STRING
+        #puts GlobalArray[1]
+        #puts GlobalArray[2]   #[0] for (name,counter) [0][0] for name [0][1] for counter
       end
     end
+=end
   end
   helper_method :ingredSearch
 
   def show
     #params[:ingredient]
     @string = "water"
-    @stringarr = ['gin','water']
+    @stringarr = ['light rum','ginger beer']
     #puts @string
     counter =1
-    @resultsArry= Array.new
-    @checkArray = Array.new
+    @loopcounter=0 #we storefirst
+    @found = 0
+    @GlobalArray = Array.new
+    @LoopArray = Array.new
     @hash = Hash.new
     @stringarr.each do |item|
+      #puts @loopcounter
       puts item
       url = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{item}"
       uri = URI(url)
       response = Net::HTTP.get(uri)
       parsed = JSON.parse(response)
       if parsed['drinks'] == nil
+        puts error
         redirect_to error_404_url
+      elsif @loopcounter == 0 # first loop only
+        puts "entered first loop"
+        @loopcounter = @loopcounter+1
+        @results = parsed['drinks']
+        @results.each do |item|
+          @GlobalArray.push([item['strDrink'],counter])
+        end
+        #puts @GlobalArray[0][1]
       else
+        puts "entered second+ loop"
         if parsed['drinks'][0]['strDrink'] != item
           @results = parsed['drinks']
           @results.each do |item|
-            @checkArray.push([item['strDrink'],counter])
+            @LoopArray.push([item['strDrink'],counter])
+          end
+          @LoopArray.each do |item|
 
+            #puts "here"
+=begin            puts item[0]
+            puts @GlobalArray[@incounter]
+            index = @GlobalArray[@incounter].index(item[0])
+            puts "we are here: #{index}"
+            if index == nil #not in globalarray
+              @GlobalArray.push(item)
+            else  #index !=nil inside global
+              puts "INDEX FOUND @ " + index
+              @GlobalArray[index][1] = @GlobalArray[index][1] +1
+            end
+            @incounter = @incounter+1
+            puts @incounter
+=end
+            @GlobalArray.each do |gitem|
+              #puts item[0]
+              #puts gitem[0]
+              if item[0] == gitem[0]
+                puts "#{item[0]} == #{gitem[0]}"
+                gitem[1] = gitem[1] +1
+                @found = 1
+              end
+            end
+            if @found == 0
+              @GlobalArray.push(item)
+            end
+            @found = 0
           end
         end
-        counter = counter +1
       end
 
     end
@@ -79,14 +121,14 @@ class IngredSearchController < ApplicationController
         #puts @firstentry
         @results.each do |item|
           #puts item['strDrink']
-          @resultsArry.push([item['strDrink'],counter])
+          @GlobalArray.push([item['strDrink'],counter])
           #keyval = {'strDrink' =>item['strDrink'], 'counter' => counter}
           #puts keyval
           #@hash.merge!(keyval)
         end
-        #puts @resultsArry[0] #I HAVE DONE IT NOW I REITERATE HERE USING SECOND STRING
-        #puts @resultsArry[1]
-        #puts @resultsArry[2]   #[0] for (name,counter) [0][0] for name [0][1] for counter
+        #puts @GlobalArray[0] #I HAVE DONE IT NOW I REITERATE HERE USING SECOND STRING
+        #puts @GlobalArray[1]
+        #puts @GlobalArray[2]   #[0] for (name,counter) [0][0] for name [0][1] for counter
       #  puts @hash['strDrink']
       end
     end
