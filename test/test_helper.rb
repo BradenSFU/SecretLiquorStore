e10752dfed76955b7830dd74aefb0d5a78e6395e
Rafael <rpena@sfu.ca>
2016-12-04 21:01:46 -0800

    Minor modifications on publishes/view

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,17 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::ControllerTest
+  module CustomAssertion
+    def login(who)
+      open_session do |i|
+        i.extend(custom_assertions)
+        who = user(who)
+        i.post "/login", params: { username: who.username,
+        password: who.password }
+        assert_response :success
+      end
+    end
+  end
+end
