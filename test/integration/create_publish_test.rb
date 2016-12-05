a441eb8f5f6cb84f4dc0e05561d0c18d4c7efe18
Rafael <rpena@sfu.ca>
2016-12-05 13:54:04 -0800

    Save changes

--- test/integration/create_publish_test.rb
+++ test/integration/create_publish_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CreatePublishTest < ActionDispatch::IntegrationTest
-  test "should create publish" do
-    
+  test "should create publishes" do
+    @publish = Publish.new
+    redirect_to add_like_publish_path(@publish)
+    assert(@publish.likes.valid?)
   end
 end
