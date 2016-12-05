c04b29cd57f959ec9acad1a0b6d8ca95187b495d
DoubleLLLL <yongzhel@sfu.ca>
2016-12-04 16:24:27 -0800

    User controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,31 +3,22 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.new(Username: "can create", Email: "can@email.com", Biography: "I am can create",
-    Password: "123456",Password_confirmation: "123456", Isadmin: true)
+    Password: "123456",Password_confirmation: "123456", Isadmin: false, id: "1")
+
+  end
+  test "should not get index" do
+    get users_url
+    assert_redirected_to root_url
+    assert :success
   end
 
-
-  test "should get index" do
-    get :index
-    assert_response :success
+  test "should creat new user" do
+    get sign_up_url(@user)
+    assert :success
   end
 
-  test "should show user" do
-    get :show, {'id' => '1'}, {'user_id' => '1'}
-    get user_url(@user)
-    assert_redirected_to @user
-  end
-
-  test "should get edit" do
-    get :edit, params: {id: 1}
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should destroy user" do
-    get :show, params: {id: 1}
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
-    end
+  test "should show user profile" do
+    get users_url (@user)
+    assert :success
   end
 end
