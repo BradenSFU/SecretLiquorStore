17ee35a67b4c446dd561251fe691773fb95975db
DoubleLLLL <yongzhel@sfu.ca>
2016-12-04 18:06:23 -0800

    profile edit

--- test/integration/profile_edit_test.rb
+++ test/integration/profile_edit_test.rb
@@ -2,18 +2,14 @@ require 'test_helper'
 
 class ProfileEditTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @user = users(:UserA)
   end
 
   test "unsuccessful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name:  "",
-                                              email: "foo@invalid",
-                                              password:              "foo",
-                                              password_confirmation: "bar" } }
+    patch user_path(@user), params: { user: { name:  "",email: "foo@invalid", password: "foo", password_confirmation: "bar" } }
 
     assert_template 'users/edit'
   end
-end
 end
