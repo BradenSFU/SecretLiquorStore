9e4f979ea9716de1b5956de26348e84683a5da05
BradenSFU <bemerson@sfu.ca>
2016-12-05 19:36:21 -0800

    ingred search tests. Also fixed an issue where published weren't showing their matched ingredient count.

--- test/controllers/ingred_search_controller_test.rb
+++ test/controllers/ingred_search_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class IngredSearchControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @ingred1 = 'Applejack'
+    @ingred2 = 'Dark Rum'
+    @ingred3 = 'Light Rum'
+  end
+
+  test "searching one ingredient" do
+    get ingred_search_url(:one => @ingred1)
+    assert_response :success
+  end
+
+  test "searching multiple ingredients" do
+    get ingred_search_url(:one => @ingred1, :two => @ingred2, :three => @ingred3)
+    assert_response :success
+  end
+
+
 end
